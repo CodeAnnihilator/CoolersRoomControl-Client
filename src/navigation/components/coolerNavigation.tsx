4f8c7408613a3751c04f6c42c741124fa56105bb
Alena <a.andriukhina@wis.software>
2019-04-11 16:13:15 +0300

    feature/42: created cooler card screen

--- src/navigation/components/coolerNavigation.tsx
+++ src/navigation/components/coolerNavigation.tsx
@@ -1,14 +1,30 @@
+import React from 'react';
 import {createStackNavigator} from 'react-navigation';
+import {TouchableOpacity} from 'react-native';
 
-import CoolerCard from '../../screens/Coolers/components/CoolerCard';
-import Coolers from '../../screens/Coolers/Coolers';
+import Menu from '../../assets/images/Menu';
+
+import CoolerCard from '../../screens/Coolers/containers/CoolerCardContainer';
+import Coolers from '../../screens/Coolers/containers/CoolersContainers';
 
 export default createStackNavigator({
 	CoolerCard: {
 		screen: CoolerCard,
+		navigationOptions: {
+			title: 'Cooler',
+		},
 	},
 	Coolers: {
 		screen: Coolers,
+		navigationOptions: ({navigation}: any) => ({
+			title: 'Rooms',
+			headerLeft: () => (
+				<TouchableOpacity onPress={navigation.openDrawer}>
+					<Menu />
+				</TouchableOpacity>
+			),
+			headerLeftContainerStyle: {paddingLeft: 20},
+		}),
 	},
 }, {
 	initialRouteName: 'Coolers',
